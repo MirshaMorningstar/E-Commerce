7b69ee626fb3004596867a958f313febe60f2e4b
unknown <a.k.mirsha9@gmail.com>
2025-05-21 16:31:19 +0530

    Setup for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import ghPages from "vite-plugin-github-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/E-Commerce/", // 👈 Required for GitHub Pages deployment
+  base: "/E-Commerce/", // 👈 Required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    ghPages(), // 👈 Plugin for GitHub Pages deployment
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
